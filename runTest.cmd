7e72db69a1b24c0c5105217683464c00706b1a4b
Mark Hermeling <mark@hermeling.org>
2024-05-12 20:43:07 +0000

    script to run through the truth table

--- runTest.cmd
+++ runTest.cmd
@@ -1,2 +1,20 @@
-print (V(/Vo1))
-print (V(/Vo2))
+alter vin1 0
+alter vin2 0
+op
+print vin1 vin2, vo1, vo2 
+alter vin1 1
+alter vin1 0
+op
+print vin1 vin2, vo1, vo2 
+alter vin1 0
+alter vin2 1
+op
+print vin1 vin2, vo1, vo2 
+alter vin1 1
+alter vin2 0
+op
+print vin1 vin2, vo1, vo2 
+alter vin1 1
+alter vin2 1
+op
+print vin1 vin2, vo1, vo2 
