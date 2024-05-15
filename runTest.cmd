ea14cdc71eb3ecb6cce4c78faea3e46ebf70c409
Mark Hermeling <mark@hermeling.org>
2024-05-15 12:19:19 +0000

    Include vinc into the truth table

--- runTest.cmd
+++ runTest.cmd
@@ -3,41 +3,42 @@ alter vin1 0
 alter vin2 0
 alter vinc 0
 op
-print vin1 vin2 vo1 vo2 
+print vin1 vin2 vinc vo1 vo2 
 alter vin1 0
 alter vin2 1
 alter vinc 0
 op
-print vin1 vin2 vo1 vo2 
+print vin1 vin2 vinc vo1 vo2 
 alter vin1 1
 alter vin2 0
 alter vinc 0
 op
-print vin1 vin2 vo1 vo2 
+print vin1 vin2 vinc vo1 vo2 
 alter vin1 1
 alter vin2 1
 alter vinc 0
 op
+print vin1 vin2 vinc vo1 vo2 
 alter vin1 0
 alter vin2 0
 alter vinc 1
 op
-print vin1 vin2 vo1 vo2
+print vin1 vin2 vinc vo1 vo2 
 alter vin1 0
 alter vin2 1
 alter vinc 1
 op
-print vin1 vin2 vo1 vo2
+print vin1 vin2 vinc vo1 vo2 
 alter vin1 1
 alter vin2 0
 alter vinc 1
 op
-print vin1 vin2 vo1 vo2
+print vin1 vin2 vinc vo1 vo2 
 alter vin1 1
 alter vin2 1
 alter vinc 1
 op
-print vin1 vin2 vo1 vo2 
+print vin1 vin2 vinc vo1 vo2 
 quit
 .endc
 .end
