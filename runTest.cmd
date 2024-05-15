b2f2ba43feb592845424ea48f7b2101476454861
Mark Hermeling <mark@hermeling.org>
2024-05-15 08:09:16 -0400

    add carry test

--- runTest.cmd
+++ runTest.cmd
@@ -1,18 +1,41 @@
 .control
 alter vin1 0
 alter vin2 0
+alter vinc 0
 op
 print vin1 vin2 vo1 vo2 
 alter vin1 0
 alter vin2 1
+alter vinc 0
 op
 print vin1 vin2 vo1 vo2 
 alter vin1 1
 alter vin2 0
+alter vinc 0
 op
 print vin1 vin2 vo1 vo2 
 alter vin1 1
 alter vin2 1
+alter vinc 0
+op
+alter vin1 0
+alter vin2 0
+alter vinc 1
+op
+print vin1 vin2 vo1 vo2
+alter vin1 0
+alter vin2 1
+alter vinc 1
+op
+print vin1 vin2 vo1 vo2
+alter vin1 1
+alter vin2 0
+alter vinc 1
+op
+print vin1 vin2 vo1 vo2
+alter vin1 1
+alter vin2 1
+alter vinc 1
 op
 print vin1 vin2 vo1 vo2 
 quit
